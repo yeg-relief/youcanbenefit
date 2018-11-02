0fc6ed14b2ea2effa68d3e136b9b2ce8d103fdd3
Jared Rewerts <jaredrewerts@gmail.com>
2018-11-02 12:03:39 -0600

    Added docker pushing to build script.

--- build/build.sh
+++ build/build.sh
@@ -2,20 +2,46 @@
 
 root="$(git rev-parse --show-toplevel)"
 hash="$(git rev-parse HEAD)"
-
 echo "Project root is $root"
+images=()
 
-if [ "$1" == "frontend" ]; then
-  echo "Building frontend Docker image"
-  docker build --tag "cityofedmonton/youcanbenefit-web:$hash" --file "$root/build/frontend.Dockerfile" "$root/frontend"
-  echo "Image built successfully"
-  echo "TagIs: cityofedmonton/youcanbenefit-web:$hash"
-elif [ "$1" == "backend" ]; then
-  echo "Building backend Docker image"
-  docker build --tag "cityofedmonton/youcanbenefit-api:$hash" --file "$root/build/backend.Dockerfile" "$root/backend"
-  echo "Image built successfully"
-  echo "TagIs: cityofedmonton/youcanbenefit-api:$hash"
-else
-  echo "Invalid build type"
-  exit -1
-fi
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "Helps with build and pushing Docker images for You Can Benefit"
+      echo " "
+      echo "$package [options] application [arguments]"
+      echo " "
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "frontend                  build the frontend image"
+      echo "backend                   build the backend image"
+      echo "-p, --push                also attempt to push the images. Must come after frontend and backend."
+      exit 0
+      ;;
+    frontend)
+      echo "Building frontend Docker image"
+      docker build --tag "cityofedmonton/youcanbenefit-web:$hash" --file "$root/build/frontend.Dockerfile" "$root/frontend"
+      images+=("cityofedmonton/youcanbenefit-web:$hash")
+      shift
+      ;;
+    backend)
+      echo "Building backend Docker image"
+      docker build --tag "cityofedmonton/youcanbenefit-api:$hash" --file "$root/build/backend.Dockerfile" "$root/backend"
+      images+=("cityofedmonton/youcanbenefit-api:$hash")
+      shift
+      ;;
+    -p|--push)
+      for image in "${images[@]}"
+      do
+        echo "Pushing $image"
+        docker push "$image"
+      done
+      shift
+      ;;
+    *)
+      echo "Unknown option $1"
+      break
+      ;;
+  esac
+done
