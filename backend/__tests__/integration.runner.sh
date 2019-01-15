90de85cb6a3c9d2b79df82f70030e61b7e07f5bd
Jared Rewerts <jaredrewerts@gmail.com>
2019-01-15 14:24:06 -0700

    Improved elasticsearch monitoring.
     - added health checking to our Int tests for elasticsearch.

--- backend/__tests__/integration.runner.sh
+++ backend/__tests__/integration.runner.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+set -e
+
 # Waits for a docker container to be ready on a given port
-# Argument #1 is container name
-# Argument #2 is port
+# Argument # is port
 waitFor ()
 {
-    until [ "`docker inspect -f {{.State.Status}} $1`"=="running" ]; do
-        sleep 0.1;
-    done;
-    echo "$1 is alive, waiting for ES to be available."
-    docker inspect --format 'localhost:'$2 $1 | xargs wget --retry-connrefused --tries=15 --wait=3 --spider
-    echo "Elasticsearch is listening for connections"
-    sleep 3
+    HEALTHY=false
+    while [[ $HEALTHY != true ]]; do
+      sleep 2
+      RES_YELLOW=$(curl -s localhost:$1/_cluster/health | sed -n 's/.*\(yellow*\).*/\1/p')
+      RES_GREEN=$(curl -s localhost:$1/_cluster/health | sed -n 's/.*\(green*\).*/\1/p')
+      
+      if [ "$RES_YELLOW" == "yellow" ] || [ "$RES_GREEN" == "green" ] ; then
+        echo "Health is " $RES_YELLOW $RES_GREEN
+        HEALTHY=true
+      fi
+    done
 }
 
 export NODE_ENV=INTEGRATION_TEST
-
 DOCKER_NAME=ycbIntegrationES
 ES_PORT_1=9400
 ES_PORT_2=9500
@@ -23,9 +27,10 @@ ES_PORT_2=9500
 docker run --name $DOCKER_NAME -p $ES_PORT_1:9200 -p $ES_PORT_2:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.1.0 &
 
 echo "Waiting for container to start..."
-waitFor $DOCKER_NAME $ES_PORT_1
+waitFor $ES_PORT_1
 
 ID=$(docker inspect --format="{{.Id}}" $DOCKER_NAME)
+echo "Container is " $ID
 
 echo "Waiting for Elasticsearch to seed..."
 npx ts-node seed/run.ts
@@ -37,7 +42,6 @@ if [ ! -z ${CHECK} ] && [ "$1" == "u" ]; then
 else
     npm run integration-test
 fi
-
 
 docker kill $ID
 docker rm $DOCKER_NAME
