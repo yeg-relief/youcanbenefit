b1bbe289bc040a3232e034f6de2e0de34ccfb913
Jared Rewerts <jaredrewerts@gmail.com>
2019-01-15 17:47:33 -0700

    Added a protected programs getter.

--- backend/__tests__/tests/protected.controller.spec.ts
+++ backend/__tests__/tests/protected.controller.spec.ts
@@ -1,6 +1,6 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { ProtectedModule } from '../../src/modules/protected/protected.module'
+import { ProtectedModule } from '../../src/modules/protected'
 import { INestApplication } from '@nestjs/common';
 
 describe('/protected', () => {
@@ -15,13 +15,29 @@ describe('/protected', () => {
     await app.init();
   })
 
-  it(`GET /login will allow you to login, auth is handled by reverse proxy`, () => {
-    return request(app.getHttpServer())
-      .get('/protected/login/')
-      .expect(200)
-      .expect(res => {
-        expect(res.body.created).toBe(true)
-      })
+  describe('/login', () => {
+    it('GET /login will allow you to login, auth is handled by reverse proxy', () => {
+      return request(app.getHttpServer())
+        .get('/protected/login/')
+        .expect(200)
+        .expect(res => {
+          expect(res.body.created).toBe(true)
+        });
+    });
+  });
+
+  describe('/program -- CRUD with program datastructures', () => {
+    it(' GET /program will return all programs and their queries', () => {
+      return request(app.getHttpServer())
+        .get('/protected/program/')
+        .expect(200)
+        .expect(response => {
+            const extractedProperties = response.body.map( applicationProgramDto => applicationProgramDto.guid);
+            extractedProperties.sort( (a, b) => a.localeCompare(b));
+
+            expect(extractedProperties).toMatchSnapshot();
+        })
+    });
   });
 
   afterAll(async () => {
