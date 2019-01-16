46ab0e89c8abebe7f744f4195d0a4459a95d594b
Jared Rewerts <jaredrewerts@gmail.com>
2019-01-15 17:24:46 -0700

    Added protected login test.

---
+++ backend/__tests__/tests/protected.controller.spec.ts
@@ -0,0 +1,30 @@
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { ProtectedModule } from '../../src/modules/protected/protected.module'
+import { INestApplication } from '@nestjs/common';
+
+describe('/protected', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [ProtectedModule]
+    })
+    .compile();
+    app = module.createNestApplication();
+    await app.init();
+  })
+
+  it(`GET /login`, () => {
+    return request(app.getHttpServer())
+      .get('/protected/login/')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.created).toBe(true)
+      })
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
