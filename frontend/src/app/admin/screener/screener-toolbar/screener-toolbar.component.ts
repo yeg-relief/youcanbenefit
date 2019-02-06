7db5ac8d77039c445b4e6c847747f3ef30efa6e0
CodyGramlich <cody.gramlich@edmonton.ca>
2019-02-06 09:25:45 -0700

    Change key to label in question index

--- frontend/src/app/admin/screener/screener-toolbar/screener-toolbar.component.ts
+++ frontend/src/app/admin/screener/screener-toolbar/screener-toolbar.component.ts
@@ -93,20 +93,20 @@ export class ScreenerToolbarComponent implements OnInit {
       map(this.removeKeyType),
     )
 
-    const unusedKeys = this.form$.pipe(
-      map(screener => {
-        const questionData = screener['form'].value
-        console.log(questionData)
-        const extractKeys = id => {
-          const question = questionData[id]
-          return question.controlType === "Multiselect" ? question.multiSelectOptions.map(q => q.key) : [question.key]
-        }
+    // const unusedKeys = this.form$.pipe(
+    //   map(screener => {
+    //     const questionData = screener['form'].value
+    //     console.log(questionData)
+    //     const extractKeys = id => {
+    //       const question = questionData[id]
+    //       return question.controlType === "Multiselect" ? question.multiSelectOptions.map(q => q.key) : [question.key]
+    //     }
 
-        const keys = Object.keys(questionData).map(extractKeys).reduce((accum, keys) => [...keys, ...accum], [])
-        const unusedKeys = screener['keys'].filter(key => keys.every(screenerKey => screenerKey.name !== key.name))
-        return unusedKeys
-      })
-    )
+    //     const keys = Object.keys(questionData).map(extractKeys).reduce((accum, keys) => [...keys, ...accum], [])
+    //     const unusedKeys = screener['keys'].filter(key => keys.every(screenerKey => screenerKey.name !== key.name))
+    //     return unusedKeys
+    //   })
+    // )
     
     const keys = this.form$.pipe(
       map(screener => {
@@ -124,7 +124,7 @@ export class ScreenerToolbarComponent implements OnInit {
     combineLatest(
       questions,
       keys,
-      unusedKeys,
+      // unusedKeys,
       (questions, keys, unusedKeys) => ({...questions, keys, unusedKeys})
     ).pipe(take(1))
       .subscribe(screener => {
@@ -164,7 +164,7 @@ export class ScreenerToolbarComponent implements OnInit {
 
   private removeKeyType(screener: {[key: string]: Question_2[]}) {
     const _removeKeyType = (question: Question_2): Question => {
-      const keyName = question.key.name;
+      const keyName = question.label;
       return (<any>Object).assign({}, question, {key: keyName});
     };
 
