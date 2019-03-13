67776c87aa01f5756cf4d91d20a344e3f6da742e
CodyGramlich <cody.gramlich@edmonton.ca>
2019-03-13 15:15:28 -0600

    Add underline to allowed tags

--- backend/src/modules/page/page.service.ts
+++ backend/src/modules/page/page.service.ts
@@ -24,8 +24,8 @@ export class PageService {
     createOrUpdate(page: PageDto): Promise<any> {
         page.documents.forEach(document => {
             document.content = sanitizeHtml(document.content, {
-                allowedTags: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
-                'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+                allowedTags: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol', 
+                'nl', 'li', 'b', 'i', 'u', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
                 'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe', 'img'],
                 allowedAttributes: {
                     '*' : [ 'class', 'style' ],
